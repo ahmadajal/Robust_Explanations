7eee74a3c0cc5b07f208c7b66fea905e904e97eb
Ubuntu <ubuntu@ip-172-31-42-117.eu-west-2.compute.internal>
2021-05-06 09:17:15 +0000

    attacks to the cifar model

--- examples/attack_expl_cifar.sh
+++ examples/attack_expl_cifar.sh
@@ -7,26 +7,58 @@
 # touch output_expl_cifar/standard/add/output.log
 # python attack_expl_cifar.py --output_dir output_expl_cifar/standard/add/ --attack_type 0 0 1 --method "saliency" | tee -a output_expl_cifar/standard/add/output.log
 #
-if [ -d output_expl_cifar/standard/add_stadv ]
+# if [ -d output_expl_cifar/standard/add_stadv ]
+# then
+#   rm -r output_expl_cifar/standard/add_stadv
+# fi
+# mkdir output_expl_cifar/standard/add_stadv
+# touch output_expl_cifar/standard/add_stadv/output.log
+# python attack_expl_cifar.py --output_dir output_expl_cifar/standard/add_stadv/ --attack_type 0 1 1 --method "saliency" | tee -a output_expl_cifar/standard/add_stadv/output.log
+# #
+# if [ -d output_expl_cifar/standard/add_recolor ]
+# then
+#   rm -r output_expl_cifar/standard/add_recolor
+# fi
+# mkdir output_expl_cifar/standard/add_recolor
+# touch output_expl_cifar/standard/add_recolor/output.log
+# python attack_expl_cifar.py --output_dir output_expl_cifar/standard/add_recolor/ --attack_type 1 0 1 --method "saliency" | tee -a output_expl_cifar/standard/add_recolor/output.log
+# #
+# if [ -d output_expl_cifar/standard/add_stadv_recolor ]
+# then
+#   rm -r output_expl_cifar/standard/add_stadv_recolor
+# fi
+# mkdir output_expl_cifar/standard/add_stadv_recolor
+# touch output_expl_cifar/standard/add_stadv_recolor/output.log
+# python attack_expl_cifar.py --output_dir output_expl_cifar/standard/add_stadv_recolor/ --attack_type 1 1 1 --method "saliency" | tee -a output_expl_cifar/standard/add_stadv_recolor/output.log
+###########################################################
+if [ -d output_expl_cifar/CURE/add ]
 then
-  rm -r output_expl_cifar/standard/add_stadv
+  rm -r output_expl_cifar/CURE/add
 fi
-mkdir output_expl_cifar/standard/add_stadv
-touch output_expl_cifar/standard/add_stadv/output.log
-python attack_expl_cifar.py --output_dir output_expl_cifar/standard/add_stadv/ --attack_type 0 1 1 --method "saliency" | tee -a output_expl_cifar/standard/add_stadv/output.log
+mkdir output_expl_cifar/CURE/add
+touch output_expl_cifar/CURE/add/output.log
+python attack_expl_cifar.py --output_dir output_expl_cifar/CURE/add/ --attack_type 0 0 1 --method "saliency" --model_path "../notebooks/models/RN18_CURE.pth" | tee -a output_expl_cifar/CURE/add/output.log
 #
-if [ -d output_expl_cifar/standard/add_recolor ]
+if [ -d output_expl_cifar/CURE/add_stadv ]
 then
-  rm -r output_expl_cifar/standard/add_recolor
+  rm -r output_expl_cifar/CURE/add_stadv
 fi
-mkdir output_expl_cifar/standard/add_recolor
-touch output_expl_cifar/standard/add_recolor/output.log
-python attack_expl_cifar.py --output_dir output_expl_cifar/standard/add_recolor/ --attack_type 1 0 1 --method "saliency" | tee -a output_expl_cifar/standard/add_recolor/output.log
+mkdir output_expl_cifar/CURE/add_stadv
+touch output_expl_cifar/CURE/add_stadv/output.log
+python attack_expl_cifar.py --output_dir output_expl_cifar/CURE/add_stadv/ --attack_type 0 1 1 --method "saliency" --model_path "../notebooks/models/RN18_CURE.pth" | tee -a output_expl_cifar/CURE/add_stadv/output.log
 #
-if [ -d output_expl_cifar/standard/add_stadv_recolor ]
+if [ -d output_expl_cifar/CURE/add_recolor ]
 then
-  rm -r output_expl_cifar/standard/add_stadv_recolor
+  rm -r output_expl_cifar/CURE/add_recolor
 fi
-mkdir output_expl_cifar/standard/add_stadv_recolor
-touch output_expl_cifar/standard/add_stadv_recolor/output.log
-python attack_expl_cifar.py --output_dir output_expl_cifar/standard/add_stadv_recolor/ --attack_type 1 1 1 --method "saliency" | tee -a output_expl_cifar/standard/add_stadv_recolor/output.log
+mkdir output_expl_cifar/CURE/add_recolor
+touch output_expl_cifar/CURE/add_recolor/output.log
+python attack_expl_cifar.py --output_dir output_expl_cifar/CURE/add_recolor/ --attack_type 1 0 1 --method "saliency" --model_path "../notebooks/models/RN18_CURE.pth" | tee -a output_expl_cifar/CURE/add_recolor/output.log
+#
+if [ -d output_expl_cifar/CURE/add_stadv_recolor ]
+then
+  rm -r output_expl_cifar/CURE/add_stadv_recolor
+fi
+mkdir output_expl_cifar/CURE/add_stadv_recolor
+touch output_expl_cifar/CURE/add_stadv_recolor/output.log
+python attack_expl_cifar.py --output_dir output_expl_cifar/CURE/add_stadv_recolor/ --attack_type 1 1 1 --method "saliency" --model_path "../notebooks/models/RN18_CURE.pth" | tee -a output_expl_cifar/CURE/add_stadv_recolor/output.log
